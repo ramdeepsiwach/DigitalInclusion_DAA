e18ce733539d2e71760fcfbd1ee64fd1971ea1a4
Ramdeep Siwach <ramdeepsiwach@Ramdeeps-MacBook-Pro.local>
2021-12-10 23:54:17 +0530

    Inisiated backend project

---
+++ DAAC_BACKEND/src/main/java/com/daac/DaacApplication.java
@@ -0,0 +1,13 @@
+package com.daac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaacApplication.class, args);
+	}
+
+}
